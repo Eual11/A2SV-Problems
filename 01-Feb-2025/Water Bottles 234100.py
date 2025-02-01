671ff6f691fedb6d2697a164c60c6eb407217119
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-02 00:55:41 +0300

    Created 'Water Bottles' by A2SV Hub

---
+++ 01-Feb-2025/Water Bottles 234100.py
@@ -0,0 +1,14 @@
+# Problem: Water Bottles - https://leetcode.com/problems/water-bottles/description
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+       num_drank = 0
+
+       while(numBottles >= numExchange):
+            numBottles-=numExchange
+            num_drank+=numExchange
+            numBottles+=1
+
+       num_drank+=numBottles
+
+       return num_drank 
