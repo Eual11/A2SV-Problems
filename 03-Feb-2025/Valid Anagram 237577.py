59ce3d212241acaad1f7a4abf202d8c2ececa919
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-03 22:44:09 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 03-Feb-2025/Valid Anagram 237577.py
@@ -0,0 +1,7 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
