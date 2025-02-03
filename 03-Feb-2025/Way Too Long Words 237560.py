174d818318ced108a32f17dc5c87f615fbb3c2d9
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-03 22:36:08 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 237560.py
@@ -0,0 +1,17 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n = int(input())
+
+res =[]
+
+for _ in range(n):
+    word = input()
+
+    if(len(word) <=10):
+        res.append(word)
+    else:
+        res.append("".join([word[0],str(len(word)-2), word[-1]]))
+
+
+for c in res:
+    print(c)
