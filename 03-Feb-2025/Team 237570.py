753d1b3ae52a4aac530aedde8bbf865c0ca96200
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-03 22:40:53 +0300

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 237570.py
@@ -0,0 +1,12 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input())
+
+
+problems =0
+
+for _ in range(n):
+    sure=sum(list(map(int, input().split())))
+    problems+= int(sure>=2)
+
+print(problems)
