f4456e610d7947e161bd50a29d7625c481e91623
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-03 22:35:49 +0300

    Created 'Young Physicist' by A2SV Hub

---
+++ 03-Feb-2025/Young Physicist 237558.py
@@ -0,0 +1,18 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+n = int(input())
+
+res =[0, 0, 0]
+
+
+for _ in range(n):
+    x, y, z = map(int, input().split())
+    res[0]+=x
+    res[1]+=y
+    res[2]+=z
+
+
+if(res ==[0,0,0]):
+    print("YES")
+else:
+    print("NO")
