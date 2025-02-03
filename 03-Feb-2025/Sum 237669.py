4db8f347a5592a86a1685a522ab0bdddf4e31fbd
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-03 23:14:39 +0300

    Created 'Sum' by A2SV Hub

---
+++ 03-Feb-2025/Sum 237669.py
@@ -0,0 +1,17 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+n = int(input())
+
+res =[]
+
+for _ in range(n):
+    l = list(map(int, input().split()))
+
+    if(sum(l)/2 in l):
+        res.append("YES")
+    else:
+        res.append("NO")
+
+
+for c in res:
+    print(c)
