64a116cb92ebe65674dc0d4b6009ab930fbdc648
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-05 18:37:10 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 05-Feb-2025/Love Story 244242.py
@@ -0,0 +1,15 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+def solve(s:str)->int:
+    t ="codeforces"
+    return sum([1 if s[i]!=t[i] else 0 for i in range(len(s))])
+
+
+n = int(input())
+res =[]
+for i in range(n):
+    s = input()
+    res.append((solve(s)))
+
+for c in res:
+    print(c)
