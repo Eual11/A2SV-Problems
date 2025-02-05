0a4fb1b2f1a2bec7ff01b5d6ad3fb83bbcdb566d
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-05 21:32:43 +0300

    Created 'Insert Delete GetRandom O(1)' by A2SV Hub

---
+++ 05-Feb-2025/Insert Delete GetRandom O(1) 244845.py
@@ -0,0 +1,37 @@
+# Problem: Insert Delete GetRandom O(1) - https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+
+import random 
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.data =[]
+        self.rand_dict ={}
+        
+
+    def insert(self, val: int) -> bool:
+        if(val not in self.rand_dict):
+            self.rand_dict[val] = len(self.data)
+            self.data.append(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+
+        if(val in self.rand_dict):
+            removed_idx = self.rand_dict[val]
+            self.rand_dict[self.data[-1]] = removed_idx
+            self.data[-1], self.data[removed_idx] = self.data[removed_idx], self.data[-1]
+            self.data.pop()
+            del self.rand_dict[val]
+            return True
+
+        return False
+        
+
+    def getRandom(self) -> int:
+
+        return random.choice(self.data)
+        
+
+
