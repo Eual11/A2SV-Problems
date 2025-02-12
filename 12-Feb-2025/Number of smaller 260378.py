6365a0b9b641227e153126ca29f54fc3d21060ea
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-12 16:45:46 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 12-Feb-2025/Number of smaller 260378.py
@@ -0,0 +1,23 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+
+
+#for while combo
+
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+res =[0]*m
+
+i=0
+for j in range(len(b)):
+
+    while i < len(a) and a[i] < b[j]:
+        i+=1
+
+    res[j]=i
+
+for c in res:
+    print(c, end= " ")
