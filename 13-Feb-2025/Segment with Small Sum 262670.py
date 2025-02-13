d598694ceba6019e720eb9799b410e90e525c195
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-13 15:15:08 +0300

    Created 'Segment with Small Sum' by A2SV Hub

---
+++ 13-Feb-2025/Segment with Small Sum 262670.py
@@ -0,0 +1,19 @@
+# Problem: Segment with Small Sum - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+
+n, s = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+l =0
+seg_len =0
+seg_sum =0
+for r in range(n):
+    while seg_sum+arr[r] > s:
+        seg_sum-=arr[l]
+        l+=1
+    seg_sum+=arr[r]
+    seg_len = max(seg_len, r-l+1)
+
+
+
+print(seg_len)
