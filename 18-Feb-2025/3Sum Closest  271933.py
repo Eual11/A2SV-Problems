502eb6f797ea312714ce8cfd970112afa322dacb
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-18 16:41:08 +0300

    Created '3Sum Closest ' by A2SV Hub

---
+++ 18-Feb-2025/3Sum Closest  271933.py
@@ -0,0 +1,37 @@
+# Problem: 3Sum Closest  - https://leetcode.com/problems/3sum-closest/
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+
+        min_diff = float('inf')
+        res =[]
+        nums.sort()
+        print(nums)
+
+        for i in range(len(nums)):
+
+            l = i+1
+            r = len(nums)-1
+
+            while l <r:
+                # print(nums[i], nums[l], nums[r])
+                diff = target-(nums[i]+nums[r]+nums[l])
+
+                # print(diff)
+                if abs(diff) < min_diff:
+                    min_diff = abs(diff)
+
+                    res =[nums[l], nums[i], nums[r]]
+
+                if diff ==0:
+                    l+=1
+                    r-=1
+                elif diff >0:
+                    l+=1
+                else:
+                    r-=1
+            
+
+
+        return sum(res)
+        
