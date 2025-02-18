e76dc348b6870869b35480a4522262f43998ef92
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-18 18:49:48 +0300

    Created 'Books' by A2SV Hub

---
+++ 18-Feb-2025/Books 272197.py
@@ -0,0 +1,19 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+max_len =0
+left =0
+seg_sum =0
+for right in range(n):
+
+    seg_sum+=arr[right]
+    while seg_sum > k:
+
+        seg_sum-=arr[left]
+        left+=1
+    max_len = max(max_len, right-left+1)
+
+
+print(max_len)
