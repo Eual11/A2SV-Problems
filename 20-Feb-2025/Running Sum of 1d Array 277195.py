b3bd40aad257d7e5783e9981526ab72f8c1d6e73
Eual <96339735+Eual11@users.noreply.github.com>
2025-02-20 19:54:49 +0300

    Created 'Running Sum of 1d Array' by A2SV Hub

---
+++ 20-Feb-2025/Running Sum of 1d Array 277195.py
@@ -0,0 +1,6 @@
+# Problem: Running Sum of 1d Array - https://leetcode.com/problems/running-sum-of-1d-array/
+
+from itertools import accumulate
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:   
+        return  list(accumulate(nums))
