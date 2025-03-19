d3844beeab5c051bba3e79ee597838b977905698
Eual <96339735+Eual11@users.noreply.github.com>
2025-03-19 22:51:39 +0300

    Created 'Validate Binary Search Tree' by A2SV Hub

---
+++ 19-Mar-2025/Validate Binary Search Tree 312753.py
@@ -0,0 +1,25 @@
+# Problem: Validate Binary Search Tree - https://leetcode.com/problems/validate-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+       arr =[] 
+
+       def inorder(root):
+        if not root:
+            return
+        inorder(root.left)
+        arr.append(root.val)
+        inorder(root.right)
+       inorder(root)
+
+
+       for i in range(0, len(arr)-1):
+        if arr[i] >= arr[i+1]:
+            return False
+       return True          
