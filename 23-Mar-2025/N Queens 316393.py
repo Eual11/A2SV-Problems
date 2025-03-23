b9f9f24b8b0142154b014ee10039364d014265e6
Eual <96339735+Eual11@users.noreply.github.com>
2025-03-23 23:40:29 +0300

    Created 'N Queens' by A2SV Hub

---
+++ 23-Mar-2025/N Queens 316393.py
@@ -0,0 +1,34 @@
+# Problem: N Queens - https://leetcode.com/problems/n-queens/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+      
+        result =[]
+        def register_board(board, n):
+            new_board = ["."*n]*n
+            for q in board:
+                row = list(new_board[q[0]])
+                row[q[1]] = "Q"
+                new_board[q[0]] = "".join(row)
+            result.append(new_board)
+        def isValid(cell, board):
+            for q in board:
+                if(q[0] == cell[0] or q[1] == cell[1] \
+                    or abs(q[0]- cell[0]) == abs(q[1] - cell[1])
+                       ):
+                    return False 
+            return True
+
+        def solve(board, row, n):
+            if(row == n):
+                register_board(board, n)
+            for col in range(n):
+                if(isValid((row, col), board)):
+                    board.append((row, col))
+                    solve(board, row+1, n)
+                    board.pop()
+
+        solve([], 0, n)
+
+        return result 
+
